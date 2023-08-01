01cd6fb32459ec6ddfd23ff741bb47656778c344
dipanshuhappy <dipanshuhappy@gmail.com>
2023-08-02 00:40:45 +0530

    fix: deployment script

--- startup.dev.sh
+++ startup.dev.sh
@@ -1,14 +1,3 @@
 #!/usr/bin/env bash
 
-export $(grep -v '^#' .env | xargs)
-
-git pull origin main
-npm run build
-npm run migration:run
-
-if [ "$NODE_ENV"== "production" ]
-then
-  pm2 start nest
-else
-  npm run start:prod
-fi
+pm2 restart nest
